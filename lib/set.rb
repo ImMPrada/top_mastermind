f71a5dd38f74fb6d498cc81c5a67801d80ba18cd
immprada <im.mprada@gmail.com>
2022-09-19 13:04:58 -0500

    add #setup_code to Set

--- lib/set.rb
+++ lib/set.rb
@@ -2,7 +2,7 @@ class Set
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-
+    @render = Render.new
     @board = Board.new
     @turn = 0
   end
@@ -13,7 +13,15 @@ class Set
   end
 
   def start
-    
-    @board.print_board
+    setup_code
+    # @board.print_board
+  end
+
+  private
+
+  def setup_code
+    @render.ask_for_code(@hacker.name)
+    @code = gets.chomp.upcase
+    @render.render_code(@code)
   end
 end
