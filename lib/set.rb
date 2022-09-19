308a5f47738e0d4b43e81dfaa3c37b4ee9f7326d
immprada <im.mprada@gmail.com>
2022-09-19 12:17:41 -0500

    add #set_roles to Set

--- lib/set.rb
+++ lib/set.rb
@@ -2,11 +2,18 @@ class Set
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+
     @board = Board.new
     @turn = 0
   end
 
+  def set_roles(encrypter, hacker)
+    @encrypter = encrypter
+    @hacker = hacker
+  end
+
   def start
+    
     @board.print_board
   end
 end
