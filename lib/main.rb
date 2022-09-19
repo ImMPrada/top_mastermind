851a76140abc0ffd45a250765515abc342749b43
immprada <im.mprada@gmail.com>
2022-09-18 20:13:32 -0500

    add Mastermind module

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,12 @@
 require 'colorize'
 require 'tty-font'
 
+require_relative 'mastermind'
 require_relative 'game'
 require_relative 'player'
 require_relative 'board'
 
 system 'clear'
-# game = Game.new
-# game.setup_game
+game = Mastermind::Game.new
+game.setup_game
 board = Board.new
---
+++ lib/mastermind.rb
@@ -0,0 +1,5 @@
+module Mastermind
+  H1_FONT = TTY::Font.new(:starwars)
+  H3_FONT = TTY::Font.new(:straight)
+  TITLE = 'MASTERMIND'.freeze
+end
