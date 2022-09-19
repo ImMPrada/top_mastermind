09bcf505fc07d45cfa4e57ad87cbd2b852ed0644
immprada <im.mprada@gmail.com>
2022-09-18 19:15:07 -0500

    add score to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,17 @@
 class Player
   def initialize(name)
     @name = name
+    @rol = nil
+    @score = 0
   end
 
   def setup
     puts 'Please enter your PIN:'.yellow
     @pin = gets.chomp
     system 'clear'
+  end
+
+  def change_rol(rol)
+    @rol = rol
   end
 end
