a46cdcd5884a0f894bacc946779021cc2be4d99c
immprada <im.mprada@gmail.com>
2022-09-19 11:57:29 -0500

    add #print_start_head to Render

--- lib/render.rb
+++ lib/render.rb
@@ -7,11 +7,27 @@ class Render
     puts H1_FONT.write(TITLE).cyan
   end
 
-  def print_roles
+  def print_start_head
+    print_title
+    puts '--------------------------------------------'.yellow
+    puts 'Welcome to Mastermind!'.yellow
+    puts 'Remember:'.yellow
+    puts "- Player1 wi'll be initial encrypter".yellow
+    puts "- you'll need PIN to reveal encrypted code".yellow
+    puts "\n"
+  end
+
+  def print_player_setup_instructions(player_caption)
+    puts H3_FONT.write("#{player_caption}:").yellow
+    puts "\n"
+    puts 'Please enter your name:'.yellow
+  end
+
+  def print_roles(player1, player2)
     puts '--------------------------------------------'.yellow
     puts 'ROLES:'.yellow
-    puts "#{@player1.name}: #{@player1.rol}".yellow
-    puts "#{@player2.name}: #{@player2.rol}".yellow
+    puts "#{player1.name}: #{player1.rol}".yellow
+    puts "#{player2.name}: #{player2.rol}".yellow
     puts '--------------------------------------------'.yellow
   end
 end
