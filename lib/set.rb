693390666665b0f90cc274deb409e7a0ec2bc0ce
immprada <im.mprada@gmail.com>
2022-09-19 13:54:13 -0500

    #setup_code to Set

--- lib/set.rb
+++ lib/set.rb
@@ -13,7 +13,8 @@ class Set
   end
 
   def start
-    setup_code
+    code_setted_up = setup_code
+    puts code_setted_up
     # @board.print_board
   end
 
@@ -22,6 +23,24 @@ class Set
   def setup_code
     @render.ask_for_code(@hacker.name)
     @code = gets.chomp.upcase
+    check = check_code(@code)
+
+    unless check[:status_ok]
+      @render.render_error_message(check[:message])
+      gets.chomp
+      setup_code
+    end
+
     @render.render_code(@code)
+    setup_code unless @render.confirm
+
+    true
+  end
+
+  def check_code(code)
+    error_message = 'Code must be 5 letters long, and contain only R, G, B, Y'
+    return { message: error_message, status_ok: false } unless code.size == 5 && code.match?(/[RGBY]{5}/)
+
+    { message: '', status_ok: true }
   end
 end
