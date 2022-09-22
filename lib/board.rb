aea3eade5810b473f2ef9720fde4f48d8c4106d2
immprada <im.mprada@gmail.com>
2022-09-22 11:13:01 -0500

    Board print the game's board

--- lib/board.rb
+++ lib/board.rb
@@ -3,10 +3,20 @@ class Board
 
   def initialize
     @board = 12.times.map { BLANK_LINE }
+    @render = Render.new
+    @board_progress = []
   end
 
-  def print_board
-    @board.each { |line| puts line }
+  def print_blank_board
+    @render.print_board(@board)
   end
+
+  def update_board(guess_code, check_result)
+    @board_progress << "#{guess_code}  ---  #{check_result}"
+    @board = @board_progress + (12 - @board_progress.length).times.map { BLANK_LINE }
+
+    @render.print_board(@board)
+  end
+
   private
 end
