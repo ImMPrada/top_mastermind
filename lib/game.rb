fc73b8c55f4d39842dc02ca87242a1e7c70749d5
immprada <im.mprada@gmail.com>
2022-09-27 15:39:21 -0500

    PR observations: Game

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,6 @@ class Game
     @sets = []
     @render = Render.new
     @score_limit = score_limit
-    @is_pair = true
   end
 
   def start
@@ -22,8 +21,11 @@ class Game
     new_set
   end
 
+  def even_round?
+    @sets.size.even?
+  end
+
   def new_set
-    @is_pair = !@is_pair
     @current_set = Set.new(@player1, @player2)
     @current_set.set_roles(@encrypter, @hacker)
     @sets << @current_set
@@ -33,27 +35,30 @@ class Game
 
   def next_set
     @encrypter.add_score(@current_set.turn)
-    return end_game if target_score_reached? && @is_pair
+    return end_game if target_score_reached? && even_round?
 
     change_roles
     new_set
   end
 
   def end_game
-    winner = hwo_won
+    winner = who_won
 
-    return @render.print_end_game_draw if winner == :draw
-
-    return @render.print_end_game(@player1, @player2) if winner == @player1
-
-    @render.print_end_game(@player2, @player1)
+    case winner
+    when @player1
+      @render.winner(@player1, @player2)
+    when @player2
+      @render.winner(@player2, @player1)
+    else
+      @render.print_end_game_draw
+    end
   end
 
   def target_score_reached?
     @player1.score >= @score_limit || @player2.score >= @score_limit
   end
 
-  def hwo_won
+  def who_won
     return @player1 if @player1.score > @player2.score
     return @player2 if @player2.score > @player1.score
 
@@ -88,26 +93,25 @@ class Game
     @render.print_name_question('PLAYER 2')
     @player2 = Player.new(gets.chomp.upcase)
     @player2.setup
-    @is_pair = true
   end
 
   def change_encrypter
     if @encrypter == @player1
       @encrypter = @player2
-      @player2.change_rol(:encrypter)
+      @player2.change_role(:encrypter)
     else
       @encrypter = @player1
-      @player1.change_rol(:encrypter)
+      @player1.change_role(:encrypter)
     end
   end
 
   def change_hacker
     if @hacker == @player2
       @hacker = @player1
-      @player1.change_rol(:hacker)
+      @player1.change_role(:hacker)
     else
       @hacker = @player2
-      @player2.change_rol(:hacker)
+      @player2.change_role(:hacker)
     end
   end
 end
